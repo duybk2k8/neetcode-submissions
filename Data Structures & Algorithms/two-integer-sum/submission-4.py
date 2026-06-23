21bed0071126a75ca4051a1876f0eff9d64bb317
Duy Bui <duybuikhanh5a0@gmail.com>
2026-06-23 21:48:40 +0700

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sum_set = set()
+        solution=[]
+        for n in nums:
+            sum_set.add(n)
+        for i in range(len(nums)):
+            if (target - nums[i]) in sum_set and i != nums.index(target - nums[i]) and i not in solution:
+                solution.append(i)
+                solution.append(nums.index(target - nums[i]))
+    
+        solution.sort()
+        return solution
